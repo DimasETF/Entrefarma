b536fab2036de37ac77637f25b3f2fba3b5e2323
DimasETF <appentrefarma@gmail.com>
2022-07-30 16:07:47 -0300

    Add spiner initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,46 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider, CircularProgress } from '@chakra-ui/react'
 import { GlobalStyle } from '../styles/global'
 
 import Link from 'next/link'
 import { PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
 import { linkResolver, repositoryName } from '../../prismicio'
+import { useEffect, useState } from 'react'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
- 
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+  }, []);
   return (
-    
-      <PrismicProvider
-        linkResolver={linkResolver}
-        internalLinkComponent={({ href, children, ...props }) => (
-          <Link href={href}>
-            <a {...props}>
-              {children}
-            </a>
-          </Link>
-        )}
-      >
-        <ChakraProvider>
-          <GlobalStyle />
-        <PrismicPreview repositoryName={repositoryName}>
-          <Component {...pageProps} />
-        </PrismicPreview>
-        </ChakraProvider>
-      </PrismicProvider>
-    
+    <>
+      {loading ? (
+        <PrismicProvider
+          linkResolver={linkResolver}
+          internalLinkComponent={({ href, children, ...props }) => (
+            <Link href={href}>
+              <a {...props}>
+                {children}
+              </a>
+            </Link>
+          )}
+        >
+          <ChakraProvider>
+            <GlobalStyle />
+            <PrismicPreview repositoryName={repositoryName}>
+              <Component {...pageProps} />
+            </PrismicPreview>
+          </ChakraProvider>
+        </PrismicProvider>
+
+      ) : (
+        <CircularProgress isIndeterminate color='green' size={"50px"} />
+      )}
+     
+    </>
   )
 }
 
